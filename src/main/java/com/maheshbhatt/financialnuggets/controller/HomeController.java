30b6dbd815f0c71b1fe12ef38a10a944b4c88739
akamahesh <mckay1718@gmail.com>
2025-11-01 10:32:59 +0530

    add user model and enhance HomeController with new endpoints

--- src/main/java/com/maheshbhatt/financialnuggets/controller/HomeController.java
+++ src/main/java/com/maheshbhatt/financialnuggets/controller/HomeController.java
@@ -1,15 +1,33 @@
 package com.maheshbhatt.financialnuggets.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.maheshbhatt.financialnuggets.model.User;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/home")
+@RequestMapping("/api")
 public class HomeController {
 
-    @GetMapping("/welcome")
+    @GetMapping("/")
     public String welcome() {
         return "Welcome to Financial Nuggets API!";
+    }
+
+    @GetMapping("/user")
+    public User user() {
+        User user = new User();
+        user.setId("1");
+        user.setName("Mahesh Bhatt");
+        user.setEmailId("maheshbhatt.dev@gmail.com");
+        return user;
+    }
+
+    @GetMapping("/path-variable/{name}")
+    public String pathVariable(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
+    @GetMapping("/request-param")
+    public String requestParam(@RequestParam String name, @RequestParam(name = "age", required = false, defaultValue = "") String age) {
+        return "Hello, " + name + " of age " + age + "!";
     }
 }
